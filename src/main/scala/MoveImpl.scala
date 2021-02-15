ba264b87534ec03988fa06321480dd9d4f1a5a21
MakarandPatare <makarandpatare@gmail.com>
2021-02-15 09:45:57 +0530

    Cosmetic changes with few more test cases.

--- src/main/scala/MoveImpl.scala
+++ src/main/scala/MoveImpl.scala
@@ -6,17 +6,17 @@ trait Move {
   def maxSteps: Int
   def directions: List[Direction]
   def moves(pos: Position, dir: Direction, remSteps: Int, posList: List[Position]): List[Position] = {
-    if (pos.file < file.head || pos.file > file.last || pos.rank < rank.head || pos.rank > rank.last || remSteps == 0)
+    if (pos.row < rows.head || pos.row > rows.last || pos.column < columns.head || pos.column > columns.last || remSteps == 0)
       posList
     else {
-      val appendedPosList = posList :+ Position(pos.file + dir.shift._1, pos.rank + dir.shift._2)
-      moves(Position(pos.file + dir.shift._1, pos.rank + dir.shift._2), dir, remSteps - 1, appendedPosList)
+      val appendedPosList = posList :+ Position(pos.row + dir.shift._1, pos.column + dir.shift._2)
+      moves(Position(pos.row + dir.shift._1, pos.column + dir.shift._2), dir, remSteps - 1, appendedPosList)
     }
   }
   def possibleMoves(pos: Position): String = {
     directions.flatMap {
       dir => moves(pos, dir, maxSteps, List[Position]())
-    }.filter(p => Chessboard.file.contains(p.file) && Chessboard.rank.contains(p.rank)).mkString(",")
+    }.filter(p => Chessboard.rows.contains(p.row) && Chessboard.columns.contains(p.column)).mkString(",")
   }
 }
 
